152ffa449fa65d7484f61f2d25cdb9b5c47bb078
Abhip32 <abhishek.a.patil112@gmail.com>
2024-09-13 23:36:49 +0530

    loader fix

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loader = () => {
   return (
-    <div className="fixed top-0 left-0 z-50 h-[100vh] w-[100vw] flex items-center justify-center bg-black bg-opacity-50">
+    <div className="fixed top-0 left-0 h-[100vh] w-[100vw] flex items-center justify-center bg-black">
       <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-blue-600" role="status">
         <span className="sr-only">Loading...</span>
       </div>
